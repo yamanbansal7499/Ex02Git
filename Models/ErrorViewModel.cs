d86615d263c03d465364435bbb3d46e3a1e9c4e3
yamanbansal7499 <yamanbansal7499@gmail.com>
2021-06-10 15:23:02 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ex02_git_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
