a6ef60d03287365eafafebccc432978819b47074
yamanbansal7499 <yamanbansal7499@gmail.com>
2021-06-10 15:24:54 +0530

    Second Commit

---
+++ Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex02_git_.Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+    }
+}
