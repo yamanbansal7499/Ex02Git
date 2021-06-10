83276b752902a1eeb83d1e30d3a55a9e340057fe
yamanbansal7499 <yamanbansal7499@gmail.com>
2021-06-10 15:29:57 +0530

    Third Commit

---
+++ Data/PlayerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ex02_git_.Models;
+
+namespace Ex02_git_.Data
+{
+    public class PlayerDbContext : DbContext
+    {
+        public PlayerDbContext (DbContextOptions<PlayerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ex02_git_.Models.Player> Player { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ex02_git_.Data;
 
 namespace Ex02_git_
 {
@@ -24,6 +26,9 @@ namespace Ex02_git_
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<PlayerDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PlayerDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
